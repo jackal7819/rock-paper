94d35141e68971ed62d7b36c3ba290766c4a6f0d
Viktor Filippov <jackal3166@gmail.com>
2024-02-10 21:29:48 +0200

    Add play

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,53 @@
 <script setup>
+	import { ref, mounted, computed } from 'vue';
+
+	const wins = ref(0);
+	const losses = ref(0);
+	const draws = ref(0);
+
+	const userChoice = ref(null);
+	const computerChoice = ref(null);
+	const verdict = ref(null);
+
+	const outcomes = {
+		rock: {
+			rock: 'draw',
+			paper: 'loss',
+			scissors: 'win',
+		},
+		paper: {
+			rock: 'win',
+			paper: 'draw',
+			scissors: 'loss',
+		},
+		scissors: {
+			rock: 'loss',
+			paper: 'win',
+			scissors: 'draw',
+		},
+	};
+
+	const play = (choice) => {
+		const choices = ['rock', 'paper', 'scissors'];
+		const random = Math.floor(Math.random() * choices.length);
+		
+		computerChoice.value = Math.floor(Math.random() * choices.length)		
+		userChoice.value = choice;
+		verdict.value = choices[random];
+
+		const outcome = outcomes[choice][verdict.value];
+		
+		if (outcome === 'win') {
+			wins.value++;
+			verdict.value = 'You win!';
+		} else if (outcome === 'loss') {
+			losses.value++;
+			verdict.value = 'You lose!';
+		} else {
+			draws.value++;
+			verdict.value = 'It is a draw!';
+		}
+	};
 </script>
 
 <template>
